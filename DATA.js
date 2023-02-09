6eea899296e71c3d6701c4d11b7dda3536f549ef
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-02-09 17:50:41 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,50 @@
+export const item = {
+    "id": "fe749c66-7e79-46cf-92f1-50f0e1b0ac60",
+    "stories": [
+        {
+            "id": "fa1d3afc-265f-4bdf-b7f4-9a325e018f72",
+            "story_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/348.png",
+            "story_name": "five",
+            "eye_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/35.png",
+            "views": 3541,
+            "play_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/7.png",
+            "about_story": "Husband discover account what. Many her child."
+        },
+        {
+            "id": "325d5904-3cf3-4d44-9623-c0a27582b461",
+            "story_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/26.png",
+            "story_name": "reveal",
+            "eye_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/149.png",
+            "views": 2670,
+            "play_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/94.png",
+            "about_story": "Become truth couple could. Minute foreign personal laugh science."
+        },
+        {
+            "id": "1b0b612d-54d1-473d-919b-949176d06761",
+            "story_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/367.png",
+            "story_name": "either",
+            "eye_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/347.png",
+            "views": 2310,
+            "play_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/257.png",
+            "about_story": "Table whether wait painting majority once. Feeling they clearly home activity. Seem be consumer blue crime couple pattern."
+        },
+        {
+            "id": "ad4f0ebe-720e-4205-a2cf-617de461df3d",
+            "story_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/41.png",
+            "story_name": "than",
+            "eye_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/154.png",
+            "views": 8454,
+            "play_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/28.png",
+            "about_story": "Walk where at ago become eye. Change entire beyond all. Nothing stock approach early."
+        },
+        {
+            "id": "4e06b366-8f87-48e2-8912-ef891c1daaed",
+            "story_cover": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/155.png",
+            "story_name": "last",
+            "eye_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/261.png",
+            "views": 8414,
+            "play_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/296.png",
+            "about_story": "Money require than carry left answer expect. Bar simply simple game."
+        }
+    ]
+}
